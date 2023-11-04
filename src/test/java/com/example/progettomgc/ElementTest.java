9e0408f0e7bff87c72a33b84be9d8437eafffa4b
lossalloss <enrico.sallei@studio.unibo.it>
2023-11-04 22:46:08 +0100

    Finished ElementTest

--- src/test/java/com/example/progettomgc/ElementTest.java
+++ src/test/java/com/example/progettomgc/ElementTest.java
@@ -2,31 +2,37 @@ package com.example.progettomgc;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ElementTest {
 
+    Element test = new Element("test", "http://unicam.it/negozio_di_elettronica#");
+
     @Test
     void getName() {
+        assertEquals("test", test.getName());
     }
 
     @Test
     void getFullName() {
+        assertEquals("http://unicam.it/negozio_di_elettronica#"+"test", test.getFullName());
     }
 
     @Test
     void getType() {
-    }
-
-    @Test
-    void setType() {
+        test.setType("test");
+        assertEquals("test", test.getType());
     }
 
     @Test
     void getObjectProperties() {
-    }
+        ArrayList<String> op = new ArrayList<>();
+        op.add("test");
+        //aggiungo la lista di OP all elemento
+        test.setObjectProperties(op);
 
-    @Test
-    void setObjectProperties() {
+        assertEquals(op, test.getObjectProperties());
     }
 }
