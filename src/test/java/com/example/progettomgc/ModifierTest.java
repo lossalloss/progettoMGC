cffcf3e37ed6b746a76b526cd0224a0089d29ac4
lossalloss <enrico.sallei@studio.unibo.it>
2023-11-04 22:34:46 +0100

    finished Modifier tests

--- src/test/java/com/example/progettomgc/ModifierTest.java
+++ src/test/java/com/example/progettomgc/ModifierTest.java
@@ -26,13 +26,48 @@ class ModifierTest {
                 assertEquals(element, elementToAdd);
             }
         }
+        test.deleteElement(elementToAdd);
     }
 
     @Test
-    void deleteElement() {
+    void deleteElement() throws FileNotFoundException {
+        // Crea un oggetto Element da aggiungere all'ontologia
+        Element delEl = new Element("TestDelete", "http://unicam.it/negozio_di_elettronica#");
+        delEl.setType(ProductType.Accessori.name());
+
+        test.addElement(delEl);
+        //controllo prima che l elemento sia stato inserito
+        ArrayList<Element> elements = ont.getElements(ProductType.Accessori.name());
+        for (Element element : elements) {
+            if (element.equals(delEl)) {
+                assertEquals(element, delEl);
+            }
+        }
+
+        test.deleteElement(delEl);
+        elements.clear();
+
+        elements = ont.getElements(ProductType.Accessori.name());
+        //controllo che l elemento non sia contenuto nella lista di elementi
+        assertFalse(elements.contains(delEl));
     }
 
     @Test
-    void addObjectProperty() {
+    void addObjectProperty() throws FileNotFoundException {
+        // Crea un oggetto Element da aggiungere all'ontologia
+        Element elementToAdd = new Element("TestElement", "http://unicam.it/negozio_di_elettronica#");
+        elementToAdd.setType(ProductType.Accessori.name());
+
+        //aggiungo l elemento
+        test.addElement(elementToAdd);
+        //aggiungo la object property all elemento
+        test.addObjectProperty(elementToAdd, "testPrice", Properties.haPrezzo.name());
+        //di conseguenza aggiungo la object property anche al elemento locale e non solo all ontologia
+        elementToAdd.setObjectProperties(ont.getProperties(elementToAdd));
+        //controllo che le object properties siano le stesse sia nel locale che nell ontologia
+        assertEquals(ont.getProperties(elementToAdd), elementToAdd.getObjectProperties());
+
+        test.deleteElement(elementToAdd);
+
     }
 }
